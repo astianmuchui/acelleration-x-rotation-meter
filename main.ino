72f6907186bec497c5d7125c8d3f94ab29e6b927
Sebastian Muchui <Sebastianmuchui79@gmail.com>
2022-08-15 11:22:12 +0300

    create

---
+++ main.ino
@@ -0,0 +1,32 @@
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+  Serial.begin(115200);
+
+  while (!mpu.begin()) {
+    Serial.println("Not connected");
+    delay(1000);
+  }
+}
+
+sensors_event_t event;
+
+void loop() {
+  mpu.getAccelerometerSensor()->getEvent(&event);
+
+  Serial.print("[");
+  Serial.print(millis());
+  Serial.print("] X: ");
+  Serial.print(event.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(event.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(event.acceleration.z);
+  Serial.println(" m/s^2");
+  delay(500);
+}
